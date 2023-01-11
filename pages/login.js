e392f525a8e5d179c1e0a9cafb3895a8575ec7ac
“dennohkim” <kimathidennis38@gmail.com>
2023-01-11 18:01:52 +0300

    feat: login page styling

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,66 @@
-import Head from "next/head";
-import Layout from "../layout/layout";
+import Head from 'next/head'
+import Layout from '../layout/layout'
+import Link from 'next/link'
+import styles from '../styles/Form.module.css';
+import Image from 'next/image'
 
-export default function Login() {
-  return (
-    <Layout>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <main>
-        <h1>Login</h1>
-      </main>
-    </Layout>
-  );
+export default function Login(){
+    return (
+        <Layout>
+
+        <Head>
+            <title>Login</title>
+        </Head>
+        
+        <section className='w-3/4 mx-auto flex flex-col gap-10'>
+            <div className="title">
+                <h1 className='text-gray-800 text-4xl font-bold py-4'>Explore</h1>
+                <p className='w-3/4 mx-auto text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, officia?</p>
+            </div>
+
+            {/* form */}
+            <form className='flex flex-col gap-5'>
+                <div className={styles.input_group}>
+                    <input 
+                    type="email"
+                    name='email'
+                    placeholder='Email'
+                    className={styles.input_text}
+                    />
+                </div>
+                <div className={styles.input_group}>
+                    <input 
+                    type="password"
+                    name='password'
+                    placeholder='password'
+                    className={styles.input_text}
+                    />
+                </div>
+
+                {/* login buttons */}
+                <div className="input-button">
+                    <button type='submit' className={styles.button}>
+                        Login
+                    </button>
+                </div>
+                <div className="input-button">
+                    <button type='button' className={styles.button_custom}>
+                        Sign In with Google <Image src={'/assets/google.svg'} width="20" height={20} ></Image>
+                    </button>
+                </div>
+                <div className="input-button">
+                    <button type='button' className={styles.button_custom}>
+                        Sign In with Github <Image src={'/assets/github.svg'} width={25} height={25}></Image>
+                    </button>
+                </div>
+            </form>
+
+            {/* bottom */}
+            <p className='text-center text-gray-400 '>
+                don't have an account yet? <Link href={'/register'} className='text-blue-700'>Sign up</Link>
+            </p>
+        </section>
+
+        </Layout>
+    )
 }
