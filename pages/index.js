5896c9f3e1981b84e26f6ab87aa1962622a33d9f
“dennohkim” <kimathidennis38@gmail.com>
2023-01-11 18:35:07 +0300

    style: fix link

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [session, setSession] = useState(true)
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Home Page</title>
       </Head>
 
-      <main className={styles.main}>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </main>
+      {session ? User() : Guest()}
     </div>
+  )
+}
+
+// Guest
+function Guest(){
+  return (
+    <main className="container mx-auto text-center py-20">
+          <h3 className='text-4xl font-bold'>Guest Homepage</h3>
+
+          <div className='flex justify-center'>
+            <Link href={'/login'} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>Sign In</Link>
+          </div>
+      </main>
+  )
+}
+
+// Authorize User
+function User(){
+  return(
+    <main className="container mx-auto text-center py-20">
+          <h3 className='text-4xl font-bold'>Authorize User Homepage</h3>
+
+          <div className='details'>
+            <h5>Unknown</h5>
+            <h5>Unknown</h5>
+          </div>
+
+          <div className="flex justify-center">
+            <button className='mt-5 px-10 py-1 rounded-sm  bg-gray-300'>Sign Out</button>
+          </div>
+
+          <div className='flex justify-center'>
+            <Link href={'/profile'} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>Profile Page</Link>
+          </div>
+      </main>
   )
 }
